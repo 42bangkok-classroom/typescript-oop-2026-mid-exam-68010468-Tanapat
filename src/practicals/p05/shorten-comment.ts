aad579ec7918686afcef017af8d29e14ceb83736
68010468-Tanapat <68010468@kmtl.ac.th>
2026-01-21 16:32:14 +0700

    15

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -4,9 +4,10 @@ export function shortenComment(comment: string): string {
   const spit = comment.split(" ");
   for(let i = 0;i < spit.length; i ++){
     if(spit[i].length >= 5 && spit[i].length <= 10){
-      result += `${spit[i]} `
+     result += `${spit[i]} `
     }
   }
-  return(result);
+  const output = result.trim()
+  return(output);
 
 }
