415eac52f4830bca9b585b28dc18593031774570
68010468-Tanapat <68010468@kmtl.ac.th>
2026-01-21 15:24:02 +0700

    5

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,21 @@
 // Write your code below
+export{}
+
+const input: number = Number(process.argv[2]);
+console.log(typeof(input))
+if (isNaN(input) || input < 1 || !Number.isInteger(input)) {
+    process.exit()
+} else {
+
+    for (let i = input; i > 0; i--) {
+        let line = ""
+        for(let k = i; k > 0; k--){
+            line += " "
+        }
+        for (let j = 0 ; j < i; j++) {
+
+            line += j;
+        }
+        console.log(line)
+    }
+}
