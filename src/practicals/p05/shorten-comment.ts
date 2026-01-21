4bb8f40cf915f84ca7386ec40a2df507fe12daec
68010468-Tanapat <68010468@kmtl.ac.th>
2026-01-21 16:08:15 +0700

    11

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,14 @@
+export
 const comment = "TypeScript is a strongly typed programming language"
+const spit = comment.split(" ");
+console.log(spit.length)
+let result : string = "";
+export function shortenComment(comment: string): string {
+  for(let i = 0;i < spit.length; i ++){
+    if(spit[i].length >= 5 && spit[i].length <= 10){
+      result += spit[i]
+    }
+  }
+  return(result);
 
-function shortenComment(comment: string): string {
-  // Write your code below
 }
