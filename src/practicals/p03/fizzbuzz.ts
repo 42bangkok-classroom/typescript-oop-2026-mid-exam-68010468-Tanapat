9ae7508ec8f2bc38f41b15f997de40fd7785f64c
68010468-Tanapat <68010468@kmtl.ac.th>
2026-01-21 15:42:48 +0700

    9

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -5,11 +5,11 @@ if(isNaN(n)|| n < 1 ||!Number.isInteger(n)){
 }
 else{
     for(let i = 1;i<=n;i++){
-        if(i % 3 ===0 && i % 7 ===0){
+        if(i % 3 ===0 && i % 5 ===0){
             console.log("FizzBuzz");
         } else if(i % 3 === 0){
             console.log("Fizz");
-        }else if(i % 7 === 0){
+        }else if(i % 5 === 0){
             console.log("Buzz");
         }
         else{
