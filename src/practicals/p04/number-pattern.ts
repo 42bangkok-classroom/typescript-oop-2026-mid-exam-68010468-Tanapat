881d4401718b8e0d38a3b7e32ce0558b8ff22cd6
68010468-Tanapat <68010468@kmtl.ac.th>
2026-01-21 15:49:19 +0700

    9

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -3,17 +3,18 @@ export{}
 
 const input: number = Number(process.argv[2]);
 console.log(typeof(input))
+let plus = 0;
 if (isNaN(input) || input < 1 || !Number.isInteger(input)) {
     process.exit()
 } else {
 
     for (let i = input; i > 0; i--) {
         let line = ""
+        plus += 1;
         for(let k = i; k > 0; k--){
             line += " "
         }
-        for (let j = 0 ; j < i; j++) {
-
+        for (let j = plus ; j > 0; j--) {
             line += j;
         }
         console.log(line)
